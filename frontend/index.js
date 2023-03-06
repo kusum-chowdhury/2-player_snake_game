e2c3980abd9b65168768d69824d407845344af09
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-03-06 21:34:37 +0530

    added frontend

---
+++ frontend/index.js
@@ -0,0 +1,37 @@
+const BACKGROUND_COLOR = "#689DBD";
+const PLAYER_ONE_COLOR = "#8D8741";
+const PLAYER_TWO_COLOR = "#FC4445";
+const FOOD_COLOR = "#FBEEC1";
+
+const socket = io("http://localhost:3000", {
+    extraHeaders: {
+        "Access-Control-Allow-Origin": "http://127.0.0.1:8000/"
+    }
+});
+
+socket.on("init", handleInit);
+socket.on("gameStart", handleGameStart);
+socket.on("gameOver", handleGameOver);
+socket.on("unknownCode", handleUnknownCode);
+socket.on("toManyPlayers", handleTooManyPlayers);
+
+const gameScreen = document.getElementById("gameScreen");
+const initialScreen = document.getElementById("initialScreen");
+const newGameBtn = document.getElementById("newGameButton");
+const joinGameBtn = document.getElementById("joinGameButton");
+const gameCodeInput = document.getElementById("gameCodeInput");
+const gameCodeDisplay = document.getElementById("gameCodeDisplay");
+
+newGameBtn.addEventListener("click", newGame);
+joinGameBtn.addEventListener("click", joinGame);
+
+function newGame(){
+    socket.emit("newGame");
+    startGame()
+}
+
+function joinGame(){
+    const code = gameCodeInput.value;
+    socket.emit("joinGame", code);
+    startGame();
+}
